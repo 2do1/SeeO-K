4906b82db41371320ca604cd4418c8040251a667
leehanjeong <rosy0111@naver.com>
2021-08-27 20:38:10 +0900

    delete unnecessary thing

--- main.py
+++
@@ -1,6 +0,0 @@
-import extract_clothes
-
-if __name__ == '__main__':
-    color = extract_clothes.find_color_name()
-    result_list = extract_clothes.find_matching_color_name(color)
-    print(result_list)
