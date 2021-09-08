0c0895f87a29ad944cd399fd6a8e74f6744685fc
Suin Song <ingenieur_song@kookmin.ac.kr>
2021-09-08 21:32:05 +0900

    update pattern recognition files
    change to new model and image preprocessing method

--- pattern_recognition_v2.py
+++ pattern_recognition_v2.py
@@ -3,56 +3,57 @@
 import cv2
 import numpy as np
 import tensorflow as tf
+from PIL import Image, ImageOps
 
 # 패턴 인식
 def pred_pattern(fw, bg):
     # 배경 제거
-    image_fw = cv2.imread(fw)
-    image_bg = cv2.imread(bg)
-    # load images
-    image_fw = cv2.resize(image_fw, (224, 224))
-    # cv2_imshow(image1)
+    cloth = Image.open(fw).convert('RGB')
+    cloth.save('../image_data/cloth.png', 'png')
 
-    image_bg = cv2.resize(image_bg, (224, 224))
+    back = Image.open(bg).convert('RGB')
+    back.save('../image_data/back.png', 'png')
+
+    image_fw = cv2.imread('../image_data/cloth.png')
+    image_bg = cv2.imread('../image_data/back.png')
+    background_removed_img = image_fw.copy()
+
     difference = cv2.subtract(image_bg, image_fw)
-    image_fw[np.where((difference == [0,0,0]).all(axis=2))] = [0,0,0]
-    # cv2_imshow(image_fw)
+    background_removed_img[np.where((difference < [100, 100, 100]).all(axis=2))] = [0, 0, 0]
+
+    cropped_img = Image.fromarray(background_removed_img)
+    bbox = cropped_img.getbbox()
+    cropped_img = cropped_img.crop(bbox)
+
+    cropped_img = cropped_img.rotate(90)
+    cropped_img = ImageOps.fit(cropped_img, (224, 224), Image.ANTIALIAS)
+    cropped_img = np.asarray(cropped_img)
+    cropped_img = (cropped_img.astype(np.float32) / 127.0) - 1
 
     # 배경 제거된 이미지를 텐서로 변환
-    input_image = tf.convert_to_tensor(image_fw, tf.dtypes.float32)
-    # print(input_image)
+    input_image = tf.convert_to_tensor(cropped_img, tf.dtypes.float32)
     input_image = tf.expand_dims(input_image, 0)
-    # print(input_image)
 
     # 모델 불러오기
-    interpreter = tf.lite.Interpreter(model_path = "./model/model_unquant.tflite")
+    interpreter = tf.lite.Interpreter(model_path = "../model/model_unquant.tflite")
     interpreter.allocate_tensors()
 
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
 
     input_shape = input_details[0]['shape']
-    # print(input_shape)
     interpreter.set_tensor(input_details[0]['index'], input_image)
 
     # 추론하기
     interpreter.invoke()
 
-    labels = ["animal", "checkered","floral", "dotted", "striped", "solid"]
+    labels = ["체크 무늬", "꽃 무늬", "줄 무늬", "민 무늬"]
     output_data = interpreter.get_tensor(output_details[0]['index'])
-    print(output_data)
-    print(output_data)
+    print("pattern predicted list", output_data)
     result = labels[np.argmax(output_data)]
-    # print(labels[np.argmax(output_data)])
 
-    # 추론 결과
     return result
 
-if __name__ == '__main__':
-    picture = "./origin.png"
-    back = "./back.png"
-    picture_bn = cv2.imread(picture)
-    back_bn = cv2.imread(back)
-
-    result = pred_pattern(picture_bn, back_bn)
-    print(result)
+if __name__ == "__main__":
+    rt = pred_pattern("cloth.jpg", "back.jpg")
+    print(rt)
