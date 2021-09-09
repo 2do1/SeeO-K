7fe311b329e64cd1b0a08447f312e2a83c63b6c8
Kim Hayeon <54922913+kimhayeon00@users.noreply.github.com>
2021-09-09 17:51:54 +0900

    Update insert_clothes.py
    insert second commit

--- src/insert_clothes.py
+++ src/insert_clothes.py
@@ -1,6 +1,15 @@
+"""
+closet table에 구별한 옷을 저장
+* @author 김하연
+* @version 1.0.0
+"""
 import pymysql
 
 def insert_clothes(feature, color):
+    """
+    :param feature(string) : 옷 종류
+    :param color(string) : 옷 색상
+    """
     db = pymysql.connect(host='35.232.131.79', user='root', password='kobot10', db='see_ot', charset='utf8')
     cursor = db.cursor(pymysql.cursors.DictCursor)
     # 어울리는 색 검색
