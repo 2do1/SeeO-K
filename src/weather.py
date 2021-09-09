68a2529f3af23795234a987374b693d2b8ab0c64
Dowon Lee <55175475+2do1@users.noreply.github.com>
2021-09-09 17:46:11 +0900

    weather_second_commit

--- src/weather.py
+++ src/weather.py
@@ -1,11 +1,25 @@
+"""
+네이버 날씨 크롤링을 통해, 현재 위치의 날씨와 오전, 오후 강수확률을 갖고와, TTS를 생성해준다.
+그리고 강수확률이 50% 이상인지 아닌지를 판단하여, 우산을 챙겨야하는지 아닌지 여부를 판단하고 TTS를 생성해준다.
+
+@author : 이도원
+@version 1.0.0
+"""
+    
 import requests
 import gtts
 from bs4 import BeautifulSoup
 
 def weather():
-    html = requests.get('https://search.naver.com/search.naver?query=날씨')  # 네이버 날씨 크롤링
+    """
+    :return now_temperature : 현재 위치의 기온, 날씨에 따른 옷 추천을 하기 위해 return 해준다.
+    """
+
+    # 네이버 날씨 크롤링
+    html = requests.get('https://search.naver.com/search.naver?query=날씨')  
     
-    soup = BeautifulSoup(html.text, 'html.parser')  # parsing 작업
+    # parsing 작업
+    soup = BeautifulSoup(html.text, 'html.parser')  
 
     weather_box = soup.find('div', {'class': 'weather_box'})
 
@@ -26,6 +40,7 @@ def weather():
     # 오후 강수 확률
     afternoon_rain_rate = rain_list[1].text
     
+    # 현재 위치의 날씨와 오전, 오후 강수확률 TTS 생성
     tts = gtts.gTTS(text=now_address + "의 현재 온도는" + now_temperature + "도." + "현재 날씨는" + today_weather + "." + "오전 강수확률은 "
                          + morning_rain_rate + "% 이고," + "오후 강수확률은" + afternoon_rain_rate + "% 입니다.", lang="ko")
     tts.save("../sound_data/weather.wav")
@@ -34,7 +49,9 @@ def weather():
     if(int(morning_rain_rate) >= 50 or int(afternoon_rain_rate) >= 50):
         tts = gtts.gTTS(text = "비 올 확률이 50% 이상이기 때문에, 우산을 챙기세요", lang="ko")
         tts.save("../sound_data/umbrella.wav")
+    # 오전 강수 확률 또는 오후 강수 확률이 50% 미만일 경우
     else:
         tts = gtts.gTTS(text = "비 올 확률이 50% 미만이기 때문에, 우산을 안챙기셔도 됩니다", lang="ko")
         tts.save("../sound_data/umbrella.wav")
-    return now_temperature
+        
+    return now_temperature 
